5e08a1fe3598127403becab14a7551a5bb8f7950
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-14 20:42:07 +0900

    コールバックの実装について学習

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,36 @@
+# class User < ApplicationRecord
+#   # before_create :ensure_has_name
+#   before_create do
+#     self.name = 'Taro' if name.blank?
+#   end
+#   after_initialize do |user|
+#     p user
+#     puts "オブジェクトを生成しました"
+#     user.name = "Taro"
+#   end
+#   private
+
+#   # def ensure_has_name
+#   #   puts "nameの値をtaroに設定します"
+#   #   self.name = 'Taro' if name.blank?
+#   # end
+# end
+
+# class User < ApplicationRecord
+#   before_create :ensure_has_name
+
+#   def ensure_has_name
+#     self.name = 'Taro' if name.blank?
+#   end
+# end
+
+# ----------上2つ、結果は同じ
+
 class User < ApplicationRecord
+  before_save do
+    puts '１番目に呼ばれたコールバック'
+  end
+  before_save do
+    puts '２番目に呼ばれたコールバック'
+  end
 end
